d611f551e0a9dd85af2feea3f9686f2ed59704cd
skarramooch <stueynz@gmail.com>
2026-06-22 11:41:32 +1200

    text_node_to_html_node options created, neeed refining, test script for each TextType

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -40,5 +40,37 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
 
+    def test_bold(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold node")
+
+    def test_italic(self):
+        node = TextNode("This is a italic node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is an italic node")
+
+    def test_code(self):
+        node = TextNode("This is a code node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a code node")
+
+    def test_link(self):
+        node = TextNode("This is a link node", TextType.LINK)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link node")
+
+    def test_image(self):
+        node = TextNode("This is an image node", TextType.IMAGE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "This is an image node")
+
+
+
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -28,6 +28,20 @@ class TextNode:
 def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.TEXT:
         return LeafNode(None, text_node.text, text_node.url)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text, text_node.url)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text, text_node.url)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text, text_node.url)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, text_node.url)
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", text_node.url)
+
     else:
         raise Exception("no chukka chukka")
+
+# TextType.LINK: "a" tag, anchor text, and "href" prop
+# TextType.IMAGE: "img" tag, empty string value, "src" and "alt" props ("src" is the image URL, "alt" is the alt text)
     
