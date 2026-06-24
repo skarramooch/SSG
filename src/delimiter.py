c0ea385f839ea5c8b56851b38608e98996a7647e
skarramooch <stueynz@gmail.com>
2026-06-24 23:04:33 +1200

    still working on delimiter 20260624

--- src/delimiter.py
+++ src/delimiter.py
@@ -7,20 +7,28 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         print(f"text_type is {text_type}")
         if text_type != "plain text":
             split_nodes.append(node)
-            print(f"text type matches - adding node to split_nodes: {split_nodes}")
+            #print(f"text type not plain text - adding node UNALTERED to split_nodes: {split_nodes}")
         for char in range(len(node.text)):
             if node.text[char] == delimiter[0]:
-                print("* detected")
+                print(f"{delimiter[0]} detected {node}")
                 char +=1
                 if len(delimiter) > 1:
                     if node.text[char] == delimiter[1]:
-                        print("** detected!")
+                        print(f"{delimiter[0:1]} detected! {node}")
                         char += 2
                     if len(delimiter) > 2:
                         if node.text[char] == delimiter[2]:
-                            print("*** detected!")
-                if delimiter == bracket_queue[-1]:
+                            print(f"{delimiter[0:2]} detected! {node}")
+                if delimiter == bracket_queue[::-1]:
+                    print(f"bracket_queue[::-1]: {bracket_queue[::-1]}")
+                    print(f"bracket_queue before removing is {bracket_queue}")
                     bracket_queue.pop(-1)
-                if delimiter != bracket_queue[-1]:
+                    print(f"bracket_queue after removing is {bracket_queue}")
+
+                if delimiter != bracket_queue[::-1]:
+                    print(f"bracket_queue[::-1]: {bracket_queue[::-1]}")
+                    print(f"bracket_queue before appending is {bracket_queue}")
                     bracket_queue.append(delimiter)
+                    print(f"bracket_queue after appending is {bracket_queue}")
+        print(" \n ******************************* \n ")
         return split_nodes
--- src/test_delimiter.py
+++ src/test_delimiter.py
@@ -5,11 +5,44 @@ from textnode import TextNode, TextType, text_node_to_html_node
 class TestDelimiter(unittest.TestCase):
     def test_none(self):
         node = TextNode("This is a plain text node", TextType.TEXT)
-        print(f"node added: {node}")
         html_node = text_node_to_html_node(node)
-        print(f"converted to html node: {html_node}")
         self.assertEqual(html_node.tag, None)
         result = split_nodes_delimiter([node], "_", TextType.TEXT)
-        print(f"ran through split nodes: {result}")
         self.assertEqual(result, [node])
+        
+    def test_bold(self):
+        print("testing bold **")
+        node = TextNode("This is a **bold** node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        result = split_nodes_delimiter([node], "**", TextType.TEXT)
+        self.assertNotEqual(result, [node])
+ 
 
+
+    def test_italic(self):
+        print("testing italic *")
+        node = TextNode("This is an *italic* node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        result = split_nodes_delimiter([node], "*", TextType.TEXT)
+        self.assertEqual(result, [node])
+ 
+
+
+    def test_code(self):
+        print("testing code `")
+        node = TextNode("This is a `code` node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        result = split_nodes_delimiter([node], "`", TextType.TEXT)
+        self.assertEqual(result, [node])
+ 
+    def test_delimiter(self):
+        print("testing delimiter xyz")
+        node = TextNode("This is a xyznodexyz", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        result = split_nodes_delimiter([node], "xyz", TextType.TEXT)
+        self.assertEqual(result, [node])
+ 
