025bc552b3f6860a24671fba20171e80adf4cf3f
Stu Young <stueynz@gmail.com>
2026-07-20 23:20:40 +1200

    tested triple line breaks

--- src/test_splitter.py
+++ src/test_splitter.py
@@ -453,15 +453,22 @@ This is the same paragraph on a new line
         )
 
 
-    def donttest_markdown_to_blocks_triples(self):
+    def test_markdown_to_blocks_triples(self):
         md = """
 This is **bolded** paragraph
+
+
 
 This is another paragraph with _italic_ text and `code` here
 This is the same paragraph on a new line
 
+
+
 - This is a list
 - with items
+
+
+
 """
         blocks = markdown_to_blocks(md)
         self.assertEqual(
