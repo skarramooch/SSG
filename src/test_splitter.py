dde92a6688ab68b36fbde387ff867f27310694ed
Stu Young <stueynz@gmail.com>
2026-07-20 23:27:49 +1200

    tested all breaks no text

--- src/test_splitter.py
+++ src/test_splitter.py
@@ -528,24 +528,26 @@ This is the same paragraph on a new line
         )
 
 
-    def test_markdown_to_blocks(self):
+    def test_markdown_to_blocks_all_newline_no_text(self):
         md = """
-This is **bolded** paragraph
 
-This is another paragraph with _italic_ text and `code` here
-This is the same paragraph on a new line
 
-- This is a list
-- with items
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
 """
         blocks = markdown_to_blocks(md)
-        self.assertEqual(
-            blocks,
-            [
-                "This is **bolded** paragraph",
-                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
-                "- This is a list\n- with items",
-            ],
-        )
+        self.assertEqual(blocks, [],)
 
 
