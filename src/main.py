f4cf3a2935a7775787f9764934f8652503fcdf24
stu young <stueynz@gmail.com>
2026-09-24 18:34:44 +1200

    added build.sh - ready for github publishing

--- src/main.py
+++ src/main.py
@@ -1,22 +1,15 @@
+from sys import argv
 from textnode import TextNode, TextType
 from static_to_public import copy_static_to_public
 from markdown_to_html import generate_pages_recursive
 
 def main():
-#    print(f"[main called]")
-#    skibbidy = TextNode("anchor text goes here", TextType.BOLD, "www.skarramooch.zapto.org")
-#    print(f"[main called] skibbidy is {skibbidy}")
-#    print(f"[main called] skibbidy text is {skibbidy.text}")
-#    print(f"[main called] skibbidy text_type is {skibbidy.text_type}")
-#    print(f"[main called] skibbidy url is {skibbidy.url}")
-#    print(skibbidy)
-    copy_static_to_public("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
-    # generate_page("content/index.md", "template.html", "public/index.html")
-    # generate_page("content/blog/glorfindel/index.md","template.html", "public/blog/glorfindel/index.html")
-    # generate_page("content/blog/tom/index.md", "template.html", "public/blog/tom/index.html")
-    # generate_page("content/blog/majesty/index.md", "template.html", "public/blog/majesty/index.html")
-    # generate_page("content/contact/index.md", "template.html", "public/contact/index.html")
-
+    if argv[0] is None:
+        basepath = "/"
+    else:
+        basepath = argv[0].rsplit("src/main.py", 1)[0]
+    print(f"[basepath] {basepath}")
+    copy_static_to_public("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 main()
